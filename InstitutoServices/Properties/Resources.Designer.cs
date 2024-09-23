363f43ed03b7d707ee0a3b1f3388aab695766f57
davidgamboa114 <davidgamboa114@gmail.com>
2024-09-23 12:51:05 -0300

    cambios coneccion

--- InstitutoServices/Properties/Resources.Designer.cs
+++ InstitutoServices/Properties/Resources.Designer.cs
@@ -61,7 +61,7 @@ namespace InstitutoServices.Properties {
         }
         
         /// <summary>
-        ///   Busca una cadena traducida similar a https://datainstituto.azurewebsites.net/api/.
+        ///   Busca una cadena traducida similar a https://datainstitutodavid.azurewebsites.net/api/.
         /// </summary>
         internal static string UrlApi {
             get {
